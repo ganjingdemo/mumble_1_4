b402f4a4031ad50415ab9ec7c9b798fdc249b5b6
ganjingdemo <ganjingdemo@gmail.com>
2025-06-01 02:15:39 +0800

    [build] Update build number to 21.

--- build_n/01_04_generate_build_file_with_installer_no_server.bat
+++ build_n/01_04_generate_build_file_with_installer_no_server.bat
@@ -1,3 +1,3 @@
 set vcpkg_root=E:\vcpkg_mumble
-cmake -G "Ninja" "-DBUILD_NUMBER=16" "-Dpackaging=ON" "-Dserver=OFF" "-DVCPKG_TARGET_TRIPLET=x64-windows-static-md" "-Dstatic=ON" "-DCMAKE_TOOLCHAIN_FILE=%vcpkg_root%\scripts\buildsystems\vcpkg.cmake" "-DIce_HOME=%vcpkg_root%\installed\x64-windows-static-md" "-DCMAKE_BUILD_TYPE=Release" ..
+cmake -G "Ninja" "-DBUILD_NUMBER=21" "-Dpackaging=ON" "-Dserver=OFF" "-DVCPKG_TARGET_TRIPLET=x64-windows-static-md" "-Dstatic=ON" "-DCMAKE_TOOLCHAIN_FILE=%vcpkg_root%\scripts\buildsystems\vcpkg.cmake" "-DIce_HOME=%vcpkg_root%\installed\x64-windows-static-md" "-DCMAKE_BUILD_TYPE=Release" ..
 pause
